94455cbbd732ecca835de4772aee0c5c418c627b
munaashe <mnashesduna@gmail.com>
2022-07-26 17:19:20 +0200

    site coming soon notice

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{marginTop: '50px'}} className="App">
+      <h1>Site Coming Soon</h1>
+    </div>
+  );
+}
+
+export default App;
