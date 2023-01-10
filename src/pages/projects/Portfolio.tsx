86c59c0ae486e165dd3be269f88d67591648ec72
Denis Siduna <mnashesduna@gmail.com>
2023-01-10 12:56:09 +0200

    add sn-eng to portfolio

--- src/pages/projects/Portfolio.tsx
+++ src/pages/projects/Portfolio.tsx
@@ -1,81 +1,153 @@
-import { Container, Grid, Typography, Card, CardMedia } from '@mui/material'
-import React from 'react'
-import skyangel from '../../assets/skyangel.png';
-import smhzw from '../../assets/smhzw.png';
-import patriotizim from '../../assets/patriotizim.png';
-import abangane from '../../assets/abangane.png';
-import choicenust from '../../assets/choicenust.png'
+import { Container, Grid, Typography, Card, CardMedia } from "@mui/material";
+import React from "react";
+import skyangel from "../../assets/skyangel.png";
+import smhzw from "../../assets/smhzw.png";
+import patriotizim from "../../assets/patriotizim.png";
+import abangane from "../../assets/abangane.png";
+import choicenust from "../../assets/choicenust.png";
+import sneng from "../../assets/sn-eng.png";
 
 const Portfolio = () => {
   return (
-    <div id='portfolio' style={{ background: '#fafafa' }}>
-      <Container maxWidth='lg' sx={{ marginTop: '30px' }}>
+    <div id="portfolio" style={{ background: "#fafafa" }}>
+      <Container maxWidth="lg" sx={{ marginTop: "30px" }}>
         <br />
-        <Typography variant='h4' align='center' sx={{ color: '#050543', marginTop: '15px' }}>
+        <Typography
+          variant="h4"
+          align="center"
+          sx={{ color: "#050543", marginTop: "15px" }}
+        >
           Portfolio
         </Typography>
-        <Grid container spacing={2} style={{ justifyContent: 'space-evenly' }}>
-          <Grid item xs={12} sm={6} md={3} lg={3} height='200px'>
-            <a target='_blank' rel='noreferrer' href='https://patriotizim.com/' >
-              <Card sx={{ display: 'flex', backgroundColor: 'transparent', border: 'none', boxShadow: 'none' }}>
+        <Grid container spacing={2} style={{ justifyContent: "space-evenly" }}>
+          <Grid item xs={12} sm={6} md={3} lg={3} height="200px">
+            <a target="_blank" rel="noreferrer" href="https://patriotizim.com/">
+              <Card
+                sx={{
+                  display: "flex",
+                  backgroundColor: "transparent",
+                  border: "none",
+                  boxShadow: "none",
+                }}
+              >
                 <CardMedia
-                  sx={{ objectFit: 'contain', ':hover': { height: '180px' } }}
-                  height='200px'
-                  component='img'
+                  sx={{ objectFit: "contain", ":hover": { opacity: "80%" } }}
+                  height="200px"
+                  component="img"
                   image={patriotizim}
-                  alt='PatriotiZim - reliable fake news'
+                  alt="PatriotiZim - reliable fake news"
                 />
               </Card>
             </a>
           </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={4} height='200px'>
-            <a target='_blank' rel='noreferrer' href='https://abanganeplatform.co.zw/' >
-              <Card sx={{ display: 'flex', backgroundColor: 'transparent', border: 'none', boxShadow: 'none' }}>
+          <Grid item xs={12} sm={6} md={4} lg={4} height="200px">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://abanganeplatform.co.zw/"
+            >
+              <Card
+                sx={{
+                  display: "flex",
+                  backgroundColor: "transparent",
+                  border: "none",
+                  boxShadow: "none",
+                }}
+              >
                 <CardMedia
-                  sx={{ objectFit: 'contain', ':hover': { height: '180px' } }}
-                  height='200px'
-                  component='img'
+                  sx={{ objectFit: "contain", ":hover": { opacity: "80%" } }}
+                  height="200px"
+                  component="img"
                   image={abangane}
-                  alt='Abangane Platforms'
+                  alt="Abangane Platforms"
                 />
               </Card>
             </a>
           </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={4} height='200px'>
-            <a target='_blank' rel='noreferrer' href='https://smhzw.org.zw/' >
-              <Card sx={{ display: 'flex', backgroundColor: 'transparent', border: 'none', boxShadow: 'none' }}>
+          <Grid item xs={12} sm={6} md={4} lg={4} height="200px">
+            <a target="_blank" rel="noreferrer" href="https://smhzw.org.zw/">
+              <Card
+                sx={{
+                  display: "flex",
+                  backgroundColor: "transparent",
+                  border: "none",
+                  boxShadow: "none",
+                }}
+              >
                 <CardMedia
-                  sx={{ objectFit: 'contain', ':hover': { height: '180px' } }}
-                  height='200px'
-                  component='img'
+                  sx={{ objectFit: "contain", ":hover": { opacity: "80%" } }}
+                  height="200px"
+                  component="img"
                   image={smhzw}
-                  alt='Student Mental Health Zimbabwe'
+                  alt="Student Mental Health Zimbabwe"
                 />
               </Card>
             </a>
           </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={4} height='200px'>
-            <a target='_blank' rel='noreferrer' href='https://skyangel.co.zw/' >
-              <Card sx={{ display: 'flex', backgroundColor: 'transparent', border: 'none', boxShadow: 'none' }}>
+          <Grid item xs={12} sm={6} md={4} lg={4} height="200px">
+            <a target="_blank" rel="noreferrer" href="https://skyangel.co.zw/">
+              <Card
+                sx={{
+                  display: "flex",
+                  backgroundColor: "transparent",
+                  border: "none",
+                  boxShadow: "none",
+                }}
+              >
                 <CardMedia
-                  sx={{ objectFit: 'contain', ':hover': { height: '180px' } }}
-                  height='200px'
-                  component='img'
+                  sx={{ objectFit: "contain", ":hover": { opacity: "80%" } }}
+                  height="200px"
+                  component="img"
                   image={skyangel}
-                  alt='SkyAngel Private Jet Charters'
+                  alt="SkyAngel Private Jet Charters"
                 />
               </Card>
             </a>
           </Grid>
-          <Grid item xs={12} sm={6} md={4} lg={4} height='180px'>
-            <a target='_blank' rel='noreferrer' href='https://choicechampions.org/' >
-              <Card sx={{ display: 'flex', backgroundColor: 'transparent', border: 'none', boxShadow: 'none' }}>
+          <Grid item xs={12} sm={6} md={4} lg={4} height="180px">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://choicechampions.org/"
+            >
+              <Card
+                sx={{
+                  display: "flex",
+                  backgroundColor: "transparent",
+                  border: "none",
+                  boxShadow: "none",
+                }}
+              >
                 <CardMedia
-                  sx={{ objectFit: 'contain', ':hover': { height: '160px' } }}
-                  height='180px'
-                  component='img'
+                  sx={{ objectFit: "contain", ":hover": { opacity: "80%" } }}
+                  height="180px"
+                  component="img"
                   image={choicenust}
-                  alt='Choice Champions'
+                  alt="Choice Champions"
+                />
+              </Card>
+            </a>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={4} height="180px">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://sn-eng.co.zw/"
+            >
+              <Card
+                sx={{
+                  display: "flex",
+                  backgroundColor: "transparent",
+                  border: "none",
+                  boxShadow: "none",
+                }}
+              >
+                <CardMedia
+                  sx={{ objectFit: "contain", ":hover": { opacity: "80%" } }}
+                  height="180px"
+                  component="img"
+                  image={sneng}
+                  alt="Siduna & Ndou Engineering PVT LTD"
                 />
               </Card>
             </a>
@@ -83,8 +155,8 @@ const Portfolio = () => {
         </Grid>
       </Container>
       <br />
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
