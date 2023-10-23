3a76a92b2c9b206017cac9b25df3237c3498d70a
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2023-10-23 21:41:08 +0200

    metadata fix

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,63 +1,41 @@
-'use client'
+import React from 'react'
+import { Metadata, ResolvingMetadata } from 'next'
+import Article from './Article'
 
-import React from 'react';
-import { wordPressAPI } from '../Articles';
-import { useMediaQuery } from 'react-responsive';
 
-const Article = ({ params }: { params: { slug: string } }) => {
-    const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
-    const [article, setArticle] = React.useState({
-        title: '',
-        content: '',
-        excerpt: '',
-        date: ''
-    });
+type Props = {
+    params: { slug: string }
+    children: React.ReactNode
+    searchParams: { [key: string]: string | string[] | undefined }
+}
 
-    React.useEffect(() => {
-        const fetchArticle = async () => {
-            try {
-                const response = await fetch(`${wordPressAPI}/slug:${params.slug}`);
-                if (response.ok) {
-                    const data = await response.json();
-                    setArticle(data);
-                } else {
-                    throw new Error('Failed to fetch article');
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        };
+export async function generateMetadata(
+    { params, searchParams }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    // read route params
+    const slug = params.slug
 
-        fetchArticle();
-    }, [params.slug]);
+    // fetch data
+    const article = await fetch(`https://public-api.wordpress.com/rest/v1.1/sites/munaashe.wordpress.com/posts/slug:${slug}`).then((res) => res.json())
 
-    const { title, content, date, excerpt } = article || {};
-    let formattedDate: string;
-    console.log(article);
-    date !== '' ?
-        formattedDate = new Date(date).toLocaleString('en-GB', {
-            year: 'numeric',
-            month: 'long',
-            day: '2-digit',
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: false
-        })
-        : formattedDate = 'Loading'
+    // optionally access and extend (rather than replace) parent metadata
+    const previousImages = (await parent).openGraph?.images || []
 
+    return {
+        title: article.title,
+        openGraph: {
+            images: ['../../assets/sidesigned.png', ...previousImages],
+        },
+    }
+}
+
+const Page = ({ params }: { params: { slug: string } }) => {
     return (
-        <div className={`flex flex-col justify-center items-center`}>
-            <div className={`flex flex-col items-center justify-center pt-40 ${isMobile ? 'w-full px-12 ' : 'w-1/2'}`}>
-                <div className='text-[32px] font-bold'>
-                    {title}
-                </div>
-                <div className='italic pt-2'>
-                    {formattedDate}
-                </div>
-                <div dangerouslySetInnerHTML={{ __html: content }} className='whitespace-normal'/>
-            </div >
-        </div >
+        <div>
+            <Article params={params} />
+        </div>
     )
 }
 
-export default Article
+export default Page
