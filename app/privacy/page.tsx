519d4a6130f1a9b41eff95e80d81891508c16a0c
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2023-10-23 16:05:30 +0200

    privacy, T n Cs pages

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import Link from 'next/link';
 
-const Privacy = () => {
+const PrivacyPolicy = () => {
   return (
-    <div className='min-h-screen'>Privacy</div>
-  )
-}
+    <div className=" min-h-screen p-8 pb-2">
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg p-6">
+        <h1 className="text-3xl font-bold mb-6 mt-32">Privacy Policy</h1>
 
-export default Privacy
+        <p className="mb-4">
+          At SiDesigned, we are committed to protecting your privacy and ensuring the security of your personal
+          information. This Privacy Policy outlines how we collect, use, and safeguard your data when you use our
+          services.
+        </p>
+
+        <p className="mb-4">
+          <strong>Information We Collect</strong>
+        </p>
+
+        <p className="mb-4">
+          We may collect personal information such as your name, email address, and contact details when you interact
+          with our website or engage with our services. We also gather non-personal information through the use of
+          cookies and similar technologies to improve our website's functionality.
+        </p>
+
+        <p className="mb-4">
+          <strong>How We Use Your Information</strong>
+        </p>
+
+        <p className="mb-4">
+          We use the collected information to provide and improve our services, communicate with you, personalize your
+          experience, and ensure the security of our systems. We may also use your information for marketing purposes
+          with your consent.
+        </p>
+
+        <p className="mb-4">
+          <strong>Information Sharing</strong>
+        </p>
+
+        <p className="mb-4">
+          We do not sell, trade, or otherwise transfer your personal information to third parties without your consent,
+          except as required by law or as necessary to provide our services. We may share your information with trusted
+          partners who assist us in operating our website and delivering our services.
+        </p>
+
+        <p className="mb-4">
+          <strong>Data Security</strong>
+        </p>
+
+        <p className="mb-4">
+          We employ industry-standard security measures to protect your personal information from unauthorized access,
+          disclosure, alteration, or destruction. However, no method of transmission over the internet or electronic
+          storage is 100% secure, and we cannot guarantee absolute security.
+        </p>
+
+        <p className="mb-4">
+          <strong>Changes to This Privacy Policy</strong>
+        </p>
+
+        <p className="mb-4">
+          SiDesigned reserves the right to update or modify this Privacy Policy at any time. We encourage you to review
+          this page periodically for any changes. Your continued use of our services after any modifications
+          constitutes your acceptance of the updated Privacy Policy.
+        </p>
+
+        <p className="mb-4">
+          If you have any questions or concerns about our Privacy Policy, please send an {' '}
+          <Link href="mailto:denis.siduna@gmail.com" className="text-blue-500 hover:text-green-700">
+            email
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-const Terms = () => {
+const TermsAndConditions = () => {
   return (
-    <div className='min-h-screen'>Terms</div>
-  )
-}
+    <div className="min-h-screen p-8">
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg p-6 mt-32">
+        <h1 className="text-3xl font-bold mb-6">Terms and Conditions</h1>
 
-export default Terms
+        <p className="mb-4">
+          Welcome to SiDesigned! These terms and conditions outline the rules and regulations for the use of our website
+          and services.
+        </p>
+
+        <p className="mb-4">
+          <strong>Acceptance of Terms</strong>
+        </p>
+
+        <p className="mb-4">
+          By accessing or using our website and services, you agree to be bound by these terms and conditions. If you do
+          not agree with any part of these terms, you may not access our website or use our services.
+        </p>
+
+        <p className="mb-4">
+          <strong>Intellectual Property</strong>
+        </p>
+
+        <p className="mb-4">
+          The content on our website, including text, graphics, logos, images, and software, is the property of
+          SiDesigned and is protected by intellectual property laws. You may not use, reproduce, or distribute any
+          portion of our website or its content without our prior written permission.
+        </p>
+
+        <p className="mb-4">
+          <strong>Disclaimer of Liability</strong>
+        </p>
+
+        <p className="mb-4">
+          Our website and services are provided on an "as is" and "as available" basis. SiDesigned does not guarantee
+          the accuracy, completeness, or timeliness of the content on our website. We shall not be held liable for any
+          direct, indirect, incidental, consequential, or punitive damages arising from your access to or use of our
+          website or services.
+        </p>
+
+        <p className="mb-4">
+          <strong>Indemnification</strong>
+        </p>
+
+        <p className="mb-4">
+          You agree to indemnify and hold SiDesigned and its affiliates, officers, directors, employees, and agents
+          harmless from any claims, liabilities, damages, losses, or expenses, including reasonable attorneys' fees,
+          arising out of your use of our website or services or your violation of these terms and conditions.
+        </p>
+
+        <p className="mb-4">
+          <strong>Changes to Terms and Conditions</strong>
+        </p>
+
+        <p className="mb-4">
+          SiDesigned reserves the right to update or modify these terms and conditions at any time. We encourage you to
+          review this page periodically for any changes. Your continued use of our website and services after any
+          modifications constitutes your acceptance of the updated terms and conditions.
+        </p>
+
+        <p className="mb-4">
+          If you have any questions or concerns about our terms and conditions,  please send an {' '}
+          <Link href="mailto:denis.siduna@gmail.com" className="text-blue-500 hover:text-green-700">
+            email
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TermsAndConditions;
