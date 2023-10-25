3ca3d97e95b226a3991dc10aeb354643cba649e3
Denis Siduna <125746473+munaashe@users.noreply.github.com>
2023-10-25 11:04:50 +0200

    add personal and freelance projects

--- app/components/landingComponents/FreelanceWork.tsx
+++ app/components/landingComponents/FreelanceWork.tsx
@@ -1,55 +1,77 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import ernoca from '../../assets/ernoca.jpg'
+import internetSol from '../../assets/internetsol.jpg'
+import procad from '../../assets/procad.png'
+import skyangel from '../../assets/skyangel.jpg'
 
 const FreelanceWork = () => {
     return (
-        <div>
+        <div className='px-4'>
             <div data-aos="zoom-in" className="mt-16 text-center">
-                <h1 className="text-darken text-2xl font-semibold">Latest Freelance Projects</h1>
-                <p className="text-gray-500 my-5">See the developments that have occurred to Skillines in the world</p>
+                <h1 className="text-3xl font-bold mb-4">Latest Freelance <span className="text-yellow-500">Projects</span></h1>
+                <p className="text-gray-500 my-5">A few of the software dev projects undertaken in the past few months</p>
             </div>
             <div data-aos="zoom-in-up" className="my-14 flex flex-col lg:flex-row lg:space-x-20">
                 <div className="lg:w-6/12">
-                    <img className="w-full mb-6" src="img/laptop-news.png" alt="" />
-                    <span className="bg-yellow-300 text-darken font-semibold px-4 py-px text-sm rounded-full">NEWS</span>
-                    <h1 className="text-gray-800 font-semibold my-3 text-xl">Class adds $30 million to its balance sheet for a Zoom-friendly edtech solution</h1>
-                    <p className="text-gray-500 mb-3">Class, launched less than a year ago by Blackboard co-founder Michael Chasen, integrates exclusively...</p>
-                    <a href="" className="underline">Read more</a>
+                    <Image
+                        className="w-full mb-6 rounded-xl"
+                        src={ernoca}
+                        alt=""
+                    />
+                    <h1 className="text-gray-800 font-semibold my-3 text-xl">Ernoca - New and Pre-Owned Cars and Spares Digital Market Place</h1>
+                    <p className="text-gray-500 mb-3">Design and dev of the Ernoca app, for selling new and preowned vehicles and accessories. Used Next.js on the UI, with Redux Toolkit Query and Tailwind. Firebase for Auth and Database.</p>
+                    <Link href='https://ernoca.vercel.app' target='_blank' className="underline">Visit Site</Link>
                 </div>
                 <div className="lg:w-7/12 flex flex-col justify-between mt-12 space-y-5 lg:space-y-0 lg:mt-0">
                     <div className="flex space-x-5">
                         <div className="w-4/12">
                             <div className="relative">
-                                <img className="rounded-xl w-full" src="img/children-laptop.png" alt="" />
-                                <span className="absolute bottom-2 right-2 bg-yellow-300 text-darken font-semibold px-4 py-px text-sm rounded-full hidden sm:block">PRESS RELEASE</span>
+                                <Image
+                                    className="rounded-xl w-full"
+                                    src={procad}
+                                    alt=""
+                                />
                             </div>
                         </div>
                         <div className="w-8/12">
-                            <h1 className="text-gray-800 text-sm sm:text-lg font-semibold">Class Technologies Inc. Closes $30 Million Series A Financing to Meet High Demand</h1>
-                            <p className="text-gray-500 my-2 sm:my-4 text-xs sm:text-md">Class Technologies Inc., the company that created Class,...</p>
+                            <h1 className="text-gray-800 text-sm sm:text-lg font-semibold">Procad Construction (PVT) (LTD). Modern Website in React & Tailwind</h1>
+                            <p className="text-gray-500 my-2 sm:my-4 text-xs sm:text-md">Designed and developed the website for Procad Construction</p>
+                            <Link href='https://procad.co.zw' target='_blank' className="underline">Visit Site</Link>
                         </div>
                     </div>
                     <div className="flex space-x-5">
                         <div className="w-4/12">
                             <div className="relative">
-                                <img className="rounded-xl w-full" src="img/girl-laptop.png" alt="" />
-                                <span className="absolute bottom-2 right-2 bg-yellow-300 text-darken font-semibold px-4 py-px text-sm rounded-full hidden sm:block">NEWS</span>
+                                <Image
+                                    className="rounded-xl w-full"
+                                    src={internetSol}
+                                    alt=""
+                                />
                             </div>
                         </div>
                         <div className="w-8/12">
-                            <h1 className="text-gray-800 text-sm sm:text-lg font-semibold">Zoom’s earliest investors are betting millions on a better Zoom for schools</h1>
-                            <p className="text-gray-500 my-2 sm:my-4 text-xs sm:text-md">Zoom was never created to be a consumer product. Nonetheless, the...</p>
+                            <h1 className="text-gray-800 text-sm sm:text-lg font-semibold">Internet Solutions - Online shop for tech devices</h1>
+                            <p className="text-gray-500 my-2 sm:my-4 text-xs sm:text-md">Designed and developed the frontend, backend and cloud services</p>
+                            <Link href='https://internetsolutions.co.zw' target='_blank' className="underline">Visit Site</Link>
                         </div>
                     </div>
                     <div className="flex space-x-5">
                         <div className="w-4/12">
                             <div className="relative">
-                                <img className="rounded-xl w-full" src="img/cat-laptop.png" alt="" />
-                                <span className="absolute bottom-2 right-2 bg-yellow-300 text-darken font-semibold px-4 py-px text-sm rounded-full hidden sm:block">NEWS</span>
+                                <Image
+                                    className="rounded-xl w-full"
+                                    src={skyangel}
+                                    alt=""
+                                />
                             </div>
                         </div>
                         <div className="w-8/12">
-                            <h1 className="text-gray-800 text-sm sm:text-lg font-semibold">Former Blackboard CEO Raises $16M to Bring LMS Features to Zoom Classrooms</h1>
-                            <p className="text-gray-500 my-2 sm:my-4 text-xs sm:text-md">This year, investors have reaped big financial returns from betting on Zoom...</p>
+                            <h1 className="text-gray-800 text-sm sm:text-lg font-semibold">SkyAngel Private Jet Charters Site & Reservations System</h1>
+                            <p className="text-gray-500 my-2 sm:my-4 text-xs sm:text-md">Booking sytem design and dev, and website design and dev too</p>
+                            <Link href='https://skyangel.co.zw' target='_blank' className="underline">Visit Site</Link>
                         </div>
                     </div>
                 </div>
--- app/components/landingComponents/Projects.tsx
+++ app/components/landingComponents/Projects.tsx
@@ -1,59 +1,48 @@
 import React from 'react'
 import Image from 'next/image'
+import rusero from '../../assets/rusero.jpg'
+import sneng from '../../assets/sneng.jpg'
+import Link from 'next/link'
 
 const Projects = () => {
     return (
         <div>
-            <div className="mt-28">
+            <div className="mt-28 w-full">
                 <div data-aos="flip-down" className="text-center max-w-screen-md mx-auto">
-                    <h1 className="text-3xl font-bold mb-4">What is<span className="text-yellow-500">Skilline?</span></h1>
-                    <p className="text-gray-500">Skilline is a platform that allows educators to create online classes whereby they can store the course materials online; manage assignments, quizzes and exams; monitor due dates; grade results and provide students with feedback all in one place.</p>
+                    <h1 className="text-3xl font-bold mb-4">Current <span className="text-yellow-500">Projects</span></h1>
+                    <p className="text-gray-500">Centrally rooted in the principle of convenience, there exists a profound faith in Africa, driving our endeavor to emerge as the catalysts for providing effortlessly accessible tech services. This, in design, shall establish the bedrock of African commerce.</p>
                 </div>
-                <div data-aos="fade-up" className="flex flex-col md:flex-row justify-center space-y-5 md:space-y-0 md:space-x-6 lg:space-x-10 mt-7">
+                <div data-aos="fade-up" className="flex flex-col md:flex-row justify-center space-y-5 md:space-y-0 md:space-x-6 lg:space-x-10 mt-7 w-full">
                     <div className="relative md:w-5/12">
-                        <Image src="/img/Rectangle 19.png" alt="" layout="fill" objectFit="cover" className="rounded-2xl" />
+                        <Image
+                            src={sneng}
+                            alt=""
+                            objectFit="cover"
+                            className="rounded-2xl"
+                        />
                         <div className="absolute bg-black bg-opacity-20 bottom-0 left-0 right-0 w-full h-full rounded-2xl">
-                            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                                <h1 className="uppercase text-white font-bold text-center text-sm lg:text-xl mb-3">FOR INSTRUCTORS</h1>
-                                <button className="rounded-full text-white border text-xs lg:text-md px-6 py-3 w-full font-medium focus:outline-none transform transition hover:scale-[1.1] duration-[300ms] ease-in-out">Start a class today</button>
-                            </div>
+                            <Link href='https://sn-eng.co.zw' target='_blank'>
+                                <div className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                                    <button className="rounded-full text-white border text-xs lg:text-md px-6 py-3 w-full font-medium focus:outline-none transform transition hover:scale-[1.1] duration-[300ms] ease-in-out">Learn more</button>
+                                </div>
+                            </Link>
                         </div>
                     </div>
-                    <div className="relative md:w-[5rem]">
-                        <Image src="/img/Rectangle 21.png" alt="" layout="fill" objectFit="cover" className="rounded-[2rem]" />
-                        <div className="absolute bg-black bg-opacity-[20%] bottom-[0] left-[0] right-[0] w-full h-full rounded-[2rem]">
-                            <div className="-translate-x-[50%] -translate-y-[50%] absolute top-[50%] left-[50%] transform">
-                                <h1 className="-translate-x-[50%] -translate-y-[50%] uppercase text-white font-bold text-center text-sm lg:text-xl mb-[3rem]">FOR STUDENTS</h1>
-                                <button style={{ background: 'rgba(35, 189, 238, 0.9)' }} className="-translate-x-[50%] -translate-y-[50%] rounded-full text-white text-xs lg:text-md px[6rem]-py[3rem]-w[100%]-font-medium-focus-outline-none-transform-transition-hover-scale[110]-duration[300ms]-ease-in-out">Enter access code</button>
-                            </div>
+                    <div className="relative md:w-5/12">
+                        <Image
+                            src={rusero}
+                            alt=""
+                            objectFit="cover"
+                            className="rounded-[2rem]"
+                        />
+                        <div className="absolute bg-black bg-opacity-20 bottom-0 left-0 right-0 w-full h-full rounded-2xl">
+                            <Link href='https://rusero.co.zw' target='_blank'>
+                                <div className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                                    <button className="rounded-full text-white border text-xs lg:text-md px-6 py-3 w-full font-medium focus:outline-none transform transition hover:scale-[1.1] duration-[300ms] ease-in-out">Learn more</button>
+                                </div>
+                            </Link>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className="sm:flex items-center sm:space-x-8 mt-36">
-                <div data-aos="fade-right" className="sm:w-1/2 relative">
-                    <div className="bg-yellow-500 rounded-full absolute w-12 h-12 z-0 -left-4 -top-3 animate-pulse"></div>
-                    <h1 className="font-semibold text-2xl relative z-50 text-darken lg:pr-10">
-                        Everything you can do in a physical classroom,{' '}
-                        <span className="text-yellow-500">you can do with Skilline</span>
-                    </h1>
-                    <p className="py-5 lg:pr-32">
-                        Skilline’s school management software helps traditional and online schools manage scheduling,
-                        attendance, payments and virtual classrooms all in one secure cloud-based system.
-                    </p>
-                    <a href="" className="underline">
-                        Learn More
-                    </a>
-                </div>
-                <div data-aos="fade-left" className="sm:w-1/2 relative mt-10 sm:mt-0">
-                    <div style={{ background: '#23BDEE' }} className="floating w-24 h-24 absolute rounded-lg z-0 -top-3 -left-3"></div>
-                    <img className="rounded-xl z-40 relative" src="/img/teacher-explaining.png" alt="" />
-                    <button className="bg-white w-14 h-14 rounded-full flex items-center justify-center absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] focus:outline-none transform transition hover:scale-[110%] duration-[300ms] ease-in-out z-[50]">
-                        <svg className="w-[5px] h-[5px] ml-[1px]" viewBox="0 0 24 28" fill="#23BDEE" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M22.5751 12.8097C23.2212 13.1983 23.2212 14.135 22.5751 14.5236L1.51538 27.1891C0.848878 27.5899 5.91205e7 27.1099 6.25202e7 26.3321L7e7 100123C7e7 .223477 .848877 -.256572 151538 .14427L22.5751 12.8097Z" />
-                        </svg>
-                    </button>
-                    <div className="bg-yellow-500 w-[40px] h-[40px] floating absolute rounded-lg z-[10] -bottom-[3px] -right-[3px]"></div>
                 </div>
             </div>
         </div>
